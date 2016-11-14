02fc8e84d61f90ea69094b4b2fb393d197b739d1
gokulkrishna01 <gkgokulkrishna33@gmail.com>
2016-11-14 20:42:28 +0530

    Adding Abstract classes

---
+++ Faculty.java
@@ -0,0 +1,5 @@
+public abstract class Faculty{
+  protected String name;
+  protected String dept;
+  protected int id;
+}
---
+++ Users.java
@@ -0,0 +1,4 @@
+public abstract class Users{
+  protected String name;
+  protected String password;
+}
