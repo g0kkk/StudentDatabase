ce4b93e554a44231afe7baef06d5a6f75d5de9a9
gokulkrishna01 <gkgokulkrishna33@gmail.com>
2016-11-14 20:53:46 +0530

    Minor changes in abstract classes

--- Users.java
+++ Users.java
@@ -2,3 +2,8 @@ public abstract class Users{
   protected String name;
   protected String password;
 }
+
+public Users(String name, String password){
+  this.name = name;
+  this.password = password;
+}
