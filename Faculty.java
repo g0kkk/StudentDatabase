20f6045b6aac99789c9420cfd5b16706992f0d0e
gokulkrishna01 <gkgokulkrishna33@gmail.com>
2016-11-14 21:09:15 +0530

    Minor changes in abstract classes

--- Faculty.java
+++ Faculty.java
@@ -3,3 +3,9 @@ public abstract class Faculty{
   protected String dept;
   protected int id;
 }
+
+public Faculty(String name, String dept, int id){
+  this.name = name;
+  this.dept = dept;
+  this.id = id;
+}
